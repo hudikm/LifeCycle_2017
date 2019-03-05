9de26f2bb0b7367baa4eba81d878f4ca9ea50258
Martin Hudik <hudikm@gmail.com>
2019-03-05 16:25:56 +0100

    Update na verziu API 28.

--- app/src/main/java/fri/uniza/sk/lifecycle___/Main2Activity.java
+++ app/src/main/java/fri/uniza/sk/lifecycle___/Main2Activity.java
@@ -1,6 +1,5 @@
 package fri.uniza.sk.lifecycle___;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,14 +12,16 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         String string = getIntent().getExtras().getString("key");
-        Toast.makeText(this, string,Toast.LENGTH_SHORT).show();
-        setResult(Activity.RESULT_OK,new Intent().putExtra("key","Data"));
-        finish();
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
+
+        findViewById(R.id.button3).setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.putExtra("resultData", "Result sata from Main2Activity");
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+
     }
 
-//    @Override
-//    public void onBackPressed() {
-//        super.onBackPressed();
-//        finish();
-//    }
+
 }
--- app/src/main/java/fri/uniza/sk/lifecycle___/MainActivity.java
+++ app/src/main/java/fri/uniza/sk/lifecycle___/MainActivity.java
@@ -1,12 +1,10 @@
 package fri.uniza.sk.lifecycle___;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.PersistableBundle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,12 +24,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.d("LifeCycle", "Create");
         checkBox = (CheckBox) findViewById(R.id.checkBox);
 
-        SharedPreferences prefReader =
-                getPreferences(MODE_PRIVATE);
-        Boolean checked =
-                prefReader.getBoolean("checkBox", false);
+        SharedPreferences prefReader = getPreferences(MODE_PRIVATE);
+        Boolean checked = prefReader.getBoolean("checkBox", false);
         checkBox.setChecked(checked);
-        ((Button) findViewById(R.id.button2)).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        outState.putCharSequence("textview", textView.getText());
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(savedInstanceState.getCharSequence("textview"));
 
     }
 
@@ -62,8 +74,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
         editor.putBoolean("checkBox", checkBox.isChecked());
         editor.commit();
-
-
     }
 
     @Override
@@ -79,8 +89,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void btnClick(View v) {
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText("Ahoj Svet!");
         Intent intent = new Intent(this, Main2Activity.class);
         intent.putExtra("key", "Ahoj!");
         startActivityForResult(intent, 222);
@@ -89,37 +97,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        String text = data.getExtras().getString("key");
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        TextView textView = (TextView) findViewById(R.id.textView);
-        outState.putCharSequence("textview", textView.getText());
-
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(savedInstanceState.getCharSequence("textview"));
+        if (requestCode == 222 && resultCode == RESULT_OK) {
+            String text = data.getExtras().getString("resultData");
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+        }
 
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.button2) {
-            EditText searchTxt = (EditText) findViewById(R.id.editText);
-            Uri uri = Uri.parse("http://www.google.com/#q=" +
-                    searchTxt.getText().toString());
+        switch (v.getId()) {
+            case R.id.button2:
+                EditText searchTxt = (EditText) findViewById(R.id.editText);
+                Uri uri = Uri.parse("http://www.google.com/#q=" +
+                        searchTxt.getText().toString());
 
-            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-            startActivity(intent);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
 
+                break;
+            case R.id.button:
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("Ahoj Svet!");
+                break;
         }
     }
 }
